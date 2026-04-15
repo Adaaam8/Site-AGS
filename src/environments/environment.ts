38c7bef0528414e6c5af3d34027b30bab11dd341
Adam <adam.guevsti@gmail.com>
2026-04-15 14:39:40 +0200

    Ajouter clés Firebase et Gemini aux fichiers d'environnement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'YOUR_FIREBASE_API_KEY',
-    authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
-    projectId: 'YOUR_FIREBASE_PROJECT_ID',
-    storageBucket: 'YOUR_FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_FIREBASE_MESSAGING_SENDER_ID',
-    appId: 'YOUR_FIREBASE_APP_ID'
+    apiKey: 'AIzaSyAHIWk_n6vBHKTi0IFU5INlYQetIpYeDcY',
+    authDomain: 'ags-concept.firebaseapp.com',
+    projectId: 'ags-concept',
+    storageBucket: 'ags-concept.firebasestorage.app',
+    messagingSenderId: '306105152447',
+    appId: '1:306105152447:web:4929d5bea14e5a3015a70a',
+    measurementId:'G-PSNXFJR5NM'
   }
 };
